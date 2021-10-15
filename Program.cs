128f5968112e19920c1ec9c2f9fb2bff456e82ec
Csaba Vadász <77699103+Csaba79-coder@users.noreply.github.com>
2021-10-15 11:50:17 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleWriteAndRead
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            // stops the program till not press enter!!!
+            Console.ReadLine();
+            
+            // the console window pops up for a second and program stops! ...
+            Console.WriteLine("This program is shutting down!");
+
+            Console.WriteLine("Hello, hello World");
+            // this stops the window till pressing any button on keyboard ...
+            Console.ReadKey();
+
+            Console.WriteLine("Hello World, new way to stop the program! How to make the program to sleep ...");
+            // this requires millisecond --> 1000 milliseconds == 1 sec
+            System.Threading.Thread.Sleep(3000);
+
+            Console.WriteLine("Press enter to contiue ...");
+            Console.ReadLine();
+
+            Console.WriteLine("Press any key to continue ...");
+            Console.ReadKey();
+
+            Console.WriteLine("Let me tell you a good joke! ...");
+            System.Threading.Thread.Sleep(3000);
+            Console.WriteLine("How does NASA organize a party?");
+            System.Threading.Thread.Sleep(3000);
+            Console.WriteLine("They planet!");
+            Console.ReadKey();
+        }
+    }
+}
